799adafe6d7d92eb4d943ac36f3693dc31101d71
ekta garg <ekta@ektas-MacBook-Air.local>
2019-09-15 21:12:08 +0530

    A seperate function to handle input of file

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -41,76 +42,74 @@ func ReadFile() {
 	}
 	var parkingLot []parkinglot.Vehicle
 	for i := 0; i < len(inputArr); i++ {
-		words := strings.Fields(inputArr[i])
-
-		//parkingLot := make([]parkinglot.Vehicle, 0)
-
-		if words[0] == "create_parking_lot" { // reading `Create parking lot`
-			if len(words) != 2 {
-				fmt.Println("Input file format is incorrect!")
-			} else {
-				//convert parking_lot number into integer
-				i1, err := strconv.Atoi(words[1])
-				if err != nil {
-					fmt.Println("Can't convert string to integer")
-					return
-				}
-
-				parkingLot = parkinglot.CreateParkingLot(0, i1)
-
-			}
-		} else if words[0] == "park" { //reading 'Add new car to parking lot'
-			//fmt.Println(cap(parkingLot))
-			if len(words) != 3 {
-				fmt.Println("Input file format is incorrect!")
-			} else {
-				vehicle := parkinglot.Vehicle{
-					RegNo:  words[1],
-					Color:  words[2],
-					Status: "park",
-				}
-				parkingLot, err = vehicle.AddToParkingLot(parkingLot)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-			}
-		} else if words[0] == "leave" { //reading 'Car leaves the parking lot'
-			if len(words) != 2 {
-				fmt.Println("Input file format is incorrect!")
-				return
-			}
-			//convert alloted number into integer
-			i1, err := strconv.Atoi(words[1])
-			if err != nil {
-				fmt.Println("Can't convert string to integer")
-				return
-			}
-
-			parkingLot, err = parkinglot.RemoveFromParkingLot(i1, parkingLot)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-
-		} else if words[0] == "status" { //list all the cars
-			if len(words) != 1 {
-				fmt.Println("Input file format is incorrect!")
-				return
-			}
-			parkinglot.ListAllVehicles(parkingLot)
-		} else { //list cars with specific requirement
-			if len(words) != 2 {
-				fmt.Println("Input file format is incorrect!")
-				return
-			}
-
-			parkinglot.ListWithQuery(parkingLot, words[0], words[1])
-
+		parkingLot, err = HandleInput(parkingLot, inputArr[i], i)
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
 	}
 }
 
-func HandleInput(inputLine string) {
+//HandleInput ... Handles each line of input file
+func HandleInput(parkingLot []parkinglot.Vehicle, inputArr string, i int) ([]parkinglot.Vehicle, error) {
+	words := strings.Fields(inputArr)
 
+	if words[0] == "create_parking_lot" { // reading `Create parking lot`
+		if len(words) != 2 {
+			return parkingLot, errors.New("input file format is incorrect")
+		}
+		//convert parking_lot number into integer
+		i1, err := strconv.Atoi(words[1])
+		if err != nil {
+			return parkingLot, errors.New("Can't convert string to integer")
+		}
+
+		parkingLot = parkinglot.CreateParkingLot(0, i1)
+		return parkingLot, nil
+
+	} else if words[0] == "park" { //reading 'Add new car to parking lot'
+		if len(words) != 3 {
+			return parkingLot, errors.New("input file format is incorrect")
+		}
+		vehicle := parkinglot.Vehicle{
+			RegNo:  words[1],
+			Color:  words[2],
+			Status: "park",
+		}
+		parkingLot, err := vehicle.AddToParkingLot(parkingLot)
+		if err != nil {
+			return parkingLot, err
+		}
+		return parkingLot, nil
+
+	} else if words[0] == "leave" { //reading 'Car leaves the parking lot'
+		if len(words) != 2 {
+			return parkingLot, errors.New("input file format is incorrect")
+		}
+		//convert alloted number into integer
+		i1, err := strconv.Atoi(words[1])
+		if err != nil {
+			return parkingLot, errors.New("Can't convert string to integer")
+		}
+
+		parkingLot, err = parkinglot.RemoveFromParkingLot(i1, parkingLot)
+		if err != nil {
+			fmt.Println(err)
+			return parkingLot, nil
+		}
+		return parkingLot, nil
+
+	} else if words[0] == "status" { //list all the cars
+		if len(words) != 1 {
+			return parkingLot, errors.New("input file format is incorrect")
+		}
+		parkinglot.ListAllVehicles(parkingLot)
+		return parkingLot, nil
+	} else { //list cars with specific requirement
+		if len(words) != 2 {
+			return parkingLot, errors.New("input file format is incorrect")
+		}
+		parkinglot.ListWithQuery(parkingLot, words[0], words[1])
+		return parkingLot, nil
+	}
 }
